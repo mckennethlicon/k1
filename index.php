63dcc31f1a1550787c7e71bd9bef688ec7e7d188
mckennethlicon <mckenneth.licon@cossette.com>
2017-07-14 15:23:34 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php include('header.php'); ?>
+<!--[if lt IE 8]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+
+<article class="article">
+
+    <figure>
+        <img src="http://assets.vogue.com/photos/55c6512e08298d8be21cf934/master/pass/_ARC0015.1366x2048.JPG">
+    </figure>
+
+    <div class="article__content">
+
+
+        <table width="100%" class="article__title">
+            <tr>
+                <td width="50%">
+                     <hgroup>
+                        <h5>Sean Michael</h5>
+                        <h2>How to Listen to Jazz</h2>
+                    </hgroup>
+                </td>
+                <td width="50%">
+                    <h3>An award winning music writer on lrarning to appreciate a sudden skronk as muh as a beautiful trill.</h3>
+                </td>
+            </tr>
+        </table>
+
+        
+                    
+       <div class="article__text">
+        We rely on flowers, like music, to express emotions that seem too raw, sacred or risqué for words. <u>There is no lingua franca in the floral world;</u> a lily would mean something quite different to a lady of the Tang court and a high-toned matron of Boston. Flowers accumulate meanings and then let them fall away. Take the rose. For Dante, it symbolized the multifoliate arrangement of divine love in heaven, yet for Gertrude Stein (“a rose is a rose is a rose”) it was proof of plain being. George Orwell, railing at the decay of his beloved English language, decried the growing preference for scientific flower names (chrysanthemum, say) over the older, more descriptive forget-me-nots and snapdragons.
+
+    Buddha found Nirvana atop a lotus bloom, and the ancient Norse believed the worlds were interwoven in the branches of <a href="#">Yggdrasil</a>, the world tree. In the modern West, obsession with a complex and blush-worthy language of flowers reached its zenith during the 19th century. Though most of us can no longer read the difference between melancholic longing and burning passion in a bouquet, certain events still send even the least poetic among us to the florist: birth, love and death. Perhaps it’s time to revive a subtler language of flowers, one that serves the quieter milestones of our emotional lives.
+
+    We rely on flowers, like music, to express emotions that seem too raw, sacred or risqué for words. There is no lingua franca in the floral world; a lily would mean something quite different to a lady of the Tang court and a high-toned matron of Boston. Flowers accumulate meanings and then let them fall away. Take the rose. For Dante, it symbolized the multifoliate arrangement of divine love in heaven, yet for Gertrude Stein (“a rose is a rose is a rose”) it was proof of plain being. George Orwell, railing at the decay of his beloved English language, decried the growing preference for scientific flower names (chrysanthemum, say) over the older, more descriptive forget-me-nots and snapdragons.
+
+    Buddha found Nirvana atop a lotus bloom, and the ancient Norse believed the worlds were interwoven in the branches of Yggdrasil, the world tree. In the modern West, obsession with a complex and blush-worthy language of flowers reached its zenith during the 19th century. Though most of us can no longer read the difference between melancholic longing and burning passion in a bouquet, certain events still send even the least poetic among us to the florist: birth, love and death. Perhaps it’s time to revive a subtler language of flowers, one that serves the quieter milestones of our emotional lives.
+       </div>
+   </div>
+
+</article>
+
+
+<?php include('footer.php'); ?>
