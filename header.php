b79e641f9757dc830b63221c7f2b2ce5856d88e7
mckennethlicon <mckenneth.licon@cossette.com>
2017-07-14 15:24:16 -0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+       <link href="https://fonts.googleapis.com/css?family=Bellefair|Montserrat:600,600i|Roboto:400,400i,700,700i|Spectral:400,400i,600,600i" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
